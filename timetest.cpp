e2bf948db055dee8328ab9b4a28be7f1cf80cbc5
Andy Yang <achyang@ucdavis.edu>
2015-04-01 16:54:25 -0700

    Create initial timetest.cpp

---
+++ timetest.cpp
@@ -0,0 +1,26 @@
+// Author: Andy Yang
+
+#include <iostream>
+#include "CPUTimer.h"
+//#include "LinkedList.cpp"
+//#include "StackAr.cpp"
+#include "dsexceptions.h"
+//#include "CursorList.cpp"
+#include "LinkedList.h"
+#include "StackAr.h"
+#include "CursorList.h"
+//#include "QueueAr.cpp"
+//#include "StackLi.cpp"
+//#include "vector.cpp"
+#include "QueueAr.h"
+#include "StackLi.h"
+#include "vector.h"
+#include "SkipList.h"
+//#include "SkipList.cpp"
+
+
+int main()
+{
+
+  return 0;
+}
