530bb695c30821f70bdaac50774cef68fe4f1b3a
Andy Yang <achyang@ucdavis.edu>
2015-04-01 17:09:22 -0700

    Add text interface for user

--- timetest.cpp
+++ timetest.cpp
@@ -1,26 +1,47 @@
 // Author: Andy Yang
 
 #include <iostream>
+#include <fstream>
 #include "CPUTimer.h"
-//#include "LinkedList.cpp"
-//#include "StackAr.cpp"
 #include "dsexceptions.h"
-//#include "CursorList.cpp"
 #include "LinkedList.h"
 #include "StackAr.h"
 #include "CursorList.h"
-//#include "QueueAr.cpp"
-//#include "StackLi.cpp"
-//#include "vector.cpp"
 #include "QueueAr.h"
 #include "StackLi.h"
 #include "vector.h"
 #include "SkipList.h"
-//#include "SkipList.cpp"
 
 
 int main()
 {
+  cout << "Filename >> ";
+  char *filename;
+  cin >> filename;
+  ifstream ifs(filename);
+
+  ifs.ignore(2560, '\n');	// ignore first comment line
+
+  int input;
+  do
+  {
+    cout << endl
+         << "      ADT Menu" << endl
+         << "0. Quit" << endl
+         << "1. LinkedList" << endl
+         << "2. CursorList" << endl
+         << "3. StackAr" << endl
+         << "4. StackLi" << endl
+         << "5. QueueAr" << endl
+         << "6. SkipList" << endl
+         << "Your Choice >> ";
+    cin >> input;
+
+    if (input < 0 || input > 6)
+      cout << "Your choice is not between 1 and 6." << endl
+           << "Please try again." << endl;
+
+  } while (input);  // while input != 0
 
   return 0;
 }
