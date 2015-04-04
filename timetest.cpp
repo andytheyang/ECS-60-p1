342adfe55da7f9a07101c991e5590e6a83339773
Manu SOdhi <mssodhi@ucdavis.edu>
2015-04-03 20:45:22 -0700

    Overall better structure

--- timetest.cpp
+++ timetest.cpp
@@ -14,15 +14,33 @@
 vector<CursorNode <int> > cursorSpace(250000);
 using namespace std;
 
-int main()
-{
-    cout << "Filename >> ";
-    char filename[10];
+void RunList(char *filename) {
+    ifstream inf(filename);
+    inf.close();
+}
+
+void RunCursorList(char *filename) {
+    ifstream inf(filename);
+}
+
+void RunStackAr(char *filename) {
+    ifstream inf(filename);
+}
+
+void RunStackLi(char *filename) {
+    ifstream inf(filename);
+}
+
+void RunQueueAr(char *filename) {
+    ifstream inf(filename);
+}
+
+void RunSkipList(char *filename) {
+    ifstream inf(filename);
+}
+
+int getChoice(){
     int input;
-    cin >> filename;
-    CPUTimer ct;
-    
-    //Display the initial menu/
     cout << endl;
     cout<< "ADT Menu" << endl;
     cout<< "0. Quit" << endl;
@@ -34,42 +52,34 @@ int main()
     cout<< "6. SkipList" << endl;
     cout<< "Your Choice >> ";
     cin >> input;
+    return input;
+}
+
+int main()
+{
+    char filename[10];
+    int choice;
+    CPUTimer ct;
     
-    //While and if statements if the input isn't correct.
-    while(input < 0 || input > 6){
-        if (input<0 || input > 6) {
-            cout << "Your choice is not between 1 and 6." << endl;
-            cout<< "Please try again." << endl;
-            cout << endl;
-            cout<< "ADT Menu" << endl;
-            cout<< "0. Quit" << endl;
-            cout<< "1. LinkedList" << endl;
-            cout<< "2. CursorList" << endl;
-            cout<< "3. StackAr" << endl;
-            cout<< "4. StackLi" << endl;
-            cout<< "5. QueueAr" << endl;
-            cout<< "6. SkipList" << endl;
-            cout<< "Your Choice >> ";
-            cin >> input;
-        }
-    }
+    cout << "Filename >> ";
+    cin >> filename;
     
-    if (input > 0){
+    do
+    {
+        choice = getChoice();
         ct.reset();
-        switch (input)
+        switch (choice)
         {
-            case 1: RunList(filename); break;
+            case 1: RunList(filename);       break;
             case 2: RunCursorList(filename); break;
-            case 3: RunStackAr(filename); break;
-            case 4: RunStackLi(filename); break;
-            case 5: RunQueueAr(filename); break;
-            case 6: RunSkipList(filename); break;
+            case 3: RunStackAr(filename);    break;
+            case 4: RunStackLi(filename);    break;
+            case 5: RunQueueAr(filename);    break;
+            case 6: RunSkipList(filename);   break;
         }
         cout << "CPU time: " << ct.cur_CPUTime() << endl;
-    }
-    else{
-        cout << "CPU time: " << 0 << endl;
-    }
+    } while(choice > 0);
+    
     return 0;
 }
 
