7711686774bf215b8ac4acb7e15befe3dc058dd9
andytheyang <achyang@ucdavis.edu>
2015-04-04 17:55:29 -0700

    Bags.cpp - add containers and bags algorithm added

--- bags.cpp
+++ bags.cpp
@@ -1,4 +1,46 @@
 // Author: Andy Yang
 // Author: Manu Sodhi
 
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "StackAr.h"
+#include "QueueAr.h"
 
+using namespace std;
+
+typedef QueueAr<short> container;
+typedef StackAr<QueueAr<short> > containerStack;
+
+int main(int argc, char *argv[])
+{
+  ifstream inf(argv[1]);
+  int size = atoi(argv[2]);
+  containerStack cStack;
+
+  short bag;
+  container currentContainer(size);	// set container size
+
+  while (inf >> bag)
+  {
+    if (!currentContainer.isFull())	// if space
+    {
+      currentContainer.push(bag);
+    }
+    else
+    {
+      cStack.push(currentContainer);	// add to stack
+      // make a new container
+    }
+  }  // while all bags
+
+  while (!cStack.isEmpty())
+  {
+    container unloaded = cStack.topAndPop();	// get and remove top container
+
+    while (!unloaded.isEmpty())
+      cout << unloaded.dequeue() << " ";
+  }  // while there are more containers
+
+   return 0;
+}  // main()
